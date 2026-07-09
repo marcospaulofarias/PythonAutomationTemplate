f0fd1c4199683771193c066fcc0f09f157bc997f
marcospaulofarias <marcospaulo_farias@outlook.com>
2026-07-09 14:35:03 -0300

    Add tests and fixes

--- tests/get_elements_uiautomation.py
+++ tests/get_elements_uiautomation.py
@@ -1,6 +1,12 @@
-from use_cases.UiAutomation import UiAutomation
+from loguru import logger
+from use_cases.UiAutomationClass import UiAutomationClass
 
 if __name__ == '__main__':
-    uiautomationclass = UiAutomation()
+    uiautomationclass = UiAutomationClass()
 
-    uiautomationclass.find_element("Window")
+    # uiautomationclass.find_element("Window")
+
+    window = uiautomationclass.find_element(element_type="Window", params={"name": "Calculador"})
+    logger.info(f'WINDOW: {window}')
+    window = uiautomationclass.find_element(element_type="Window", params={"name": "Calculadora"})
+    logger.info(f'WINDOW: {window}')
---
+++ use_cases/UiAutomationClass.py
@@ -0,0 +1,38 @@
+from loguru import logger
+import uiautomation as auto
+
+class UiAutomationClass:
+    def __init__(self) -> None:
+        self.controls = {
+            "Button": auto.ButtonControl,
+            "Edit": auto.EditControl,
+            "Window": auto.WindowControl
+        }
+    
+    def find_element(self, element_type: str, params: dict) -> auto.Control | None:
+        """Captura um elemento. params: {automationid, classname, name, depth, type}"""
+        if not self._verify_dict_params(dict_params=params):
+            raise ValueError("É necessário passar no mínimo parâmetro")
+        return self._try_element(element_type=element_type, params=params)
+
+    def _verify_dict_params(self, dict_params) -> bool:
+        """Verifica se no mínimo 1 parâmetro foi passado"""
+        if all(k is None or v is None for k, v in dict_params.items()):
+            return False
+        return True
+    
+    def _try_element(self, element_type: str, params: dict, max_search_seconds: float = 20, search_interval: float = 1.0) -> auto.WindowControl | None:
+        """Busca a janela, tentando por até max_search_seconds segundos, a cada search_interval segundos"""
+        if not element_type or element_type not in self.controls:
+            raise ValueError("Obrigatório informar o tipo do elemento")
+        element = self.controls.get(element_type)(ClassName=params.get("classname"),
+                                            Name=params.get("name"),
+                                            AutomationId=params.get("automationid"),
+                                            Depth=params.get("depth"))
+        try:
+            if element.Exists(maxSearchSeconds=max_search_seconds, searchIntervalSeconds=search_interval):
+                return element
+        except Exception as error_x:
+            logger.warning(f"Erro ao buscar a janela: {error_x}")
+        logger.error(f"Janela não encontrada após {max_search_seconds}s")
+        return None
