cc27131e01c458db1f259f6d2cb583b519bb5f40
marcospaulofarias <marcospaulo_farias@outlook.com>
2026-07-10 17:25:29 -0300

    add tests

---
+++ tests/test_date_time_utils.py
@@ -0,0 +1,4 @@
+from utils.date_time_utils import get_numeric_timestamp
+
+if __name__ == '__main__':
+    print(get_numeric_timestamp())
---
+++ tests/test_print_automation.py
@@ -0,0 +1,7 @@
+from use_cases.PrintAutomation import PrintAutomation
+
+if __name__ == '__main__':
+    printautomation = PrintAutomation()
+
+    printautomation.print_full_screen()
+    
