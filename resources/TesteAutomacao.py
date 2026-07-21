f994a66a34d2c701b89b53f5a7285d0ff634938c
marcospaulofarias <marcospaulo_farias@outlook.com>
2026-07-21 19:50:57 -0300

    Test: add browser and calculator tests for getting dollar value and multiplying another amount

---
+++ resources/TesteAutomacao.py
@@ -0,0 +1,18 @@
+from resources.BancoCentral import BancoCentral
+from resources.Calculadora import Calculadora
+
+class TesteAutomacao:
+    def __init__(self):
+        self.banco_central = BancoCentral()
+        self.calculadora = Calculadora()
+
+    def get_data_bc(self):
+        """Obtém a cotação do dólar do site do Banco Central e realiza uma multiplicação usando a calculadora."""
+        dolar_value = self.banco_central.get_dolar()
+        print(f"Valor do dólar: {dolar_value}")
+        value_multiplication = self.calculadora.multiply_dolar_value(multiply_valor="3,50", dolar_value=dolar_value)
+        print(f"Resultado da multiplicação: {value_multiplication}")
+
+if __name__ == "__main__":
+    teste_automacao = TesteAutomacao()
+    teste_automacao.get_data_bc()
