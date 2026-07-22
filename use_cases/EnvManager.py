96d2138d7cbb0c07afac16d7266d0333fa3b8217
marcospaulofarias <marcospaulo_farias@outlook.com>
2026-07-22 10:31:32 -0300

    refactor: (envmanager, pathamanager) refasimplify code

--- use_cases/EnvManager.py
+++ use_cases/EnvManager.py
@@ -3,15 +3,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class EnvManager:
-    """Gerencia as variáveis de ambiente necessárias para o funcionamento do sistema."""
+    """Gerencia as variáveis de ambiente necessárias para o funcionamento do sistema.
+    A classe carrega as variáveis de ambiente do arquivo .env e verifica se todas as variáveis necessárias estão presentes.
+    Caso alguma variável esteja ausente, uma exceção é levantada.
+    """
     def __init__(self) -> None:
-        workbooks_path = getenv("PATH_WORKBOOKS")
-        if not workbooks_path:
-            raise EnvironmentError(
-                "Variável de ambiente PATH_WORKBOOKS não definida. "
-                "Defina PATH_WORKBOOKS no arquivo .env ou no ambiente do sistema."
-            )
-
+        self.workbooks_path = getenv("PATH_WORKBOOKS")
+        env_vars = (self.workbooks_path,)
+        if any(var is None for var in env_vars):
+            raise EnvironmentError("Algumas variáveis de ambiente não foram definidas. "
+                                   "Certifique-se de que todas as variáveis necessárias estão presentes no arquivo .env."
+                                   )
         self.paths = {
-            "workbooks": workbooks_path
+            "workbooks": self.workbooks_path
         }
